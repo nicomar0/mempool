81c33f2ced90a4f7124121452ba0585cc7dfb3cc
nicomar0 <nmaronese@student.ethz.ch>
2023-11-26 19:22:20 +0100

    [snitch] Added waves for debugging

--- hardware/scripts/questa/run.tcl
+++ hardware/scripts/questa/run.tcl
@@ -9,5 +9,13 @@ if {$::env(icache_faults)  ==1} {
 
 }
 do ../scripts/questa/wave.tcl
+add wave -Group gr1_axitocache {sim:/mempool_tb/dut/i_mempool_cluster/gen_groups[1]/i_group/i_axi_interco/gen_bottom_level/gen_ro_cache/i_snitch_read_only_cache/i_axi_to_cache/*}
+add wave -Group gr1_lookup {sim:/mempool_tb/dut/i_mempool_cluster/gen_groups[1]/i_group/i_axi_interco/gen_bottom_level/gen_ro_cache/i_snitch_read_only_cache/i_lookup/*}
+add wave -Group gr1_roc {sim:/mempool_tb/dut/i_mempool_cluster/gen_groups[1]/i_group/i_axi_interco/gen_bottom_level/gen_ro_cache/i_snitch_read_only_cache/*}
+add wave -Group gr1_refill {sim:/mempool_tb/dut/i_mempool_cluster/gen_groups[1]/i_group/i_axi_interco/gen_bottom_level/gen_ro_cache/i_snitch_read_only_cache/i_refill/*}
+add wave -Group gr1_handler {sim:/mempool_tb/dut/i_mempool_cluster/gen_groups[1]/i_group/i_axi_interco/gen_bottom_level/gen_ro_cache/i_snitch_read_only_cache/i_handler/*}
+add wave -Group gr1_splittertable {sim:/mempool_tb/dut/i_mempool_cluster/gen_groups[1]/i_group/i_axi_interco/gen_bottom_level/gen_ro_cache/i_snitch_read_only_cache/i_axi_to_cache/i_axi_burst_splitter_table/*}
+add wave -Group idq {sim:/mempool_tb/dut/i_mempool_cluster/gen_groups[1]/i_group/i_axi_interco/gen_bottom_level/gen_ro_cache/i_snitch_read_only_cache/i_axi_to_cache/i_axi_burst_splitter_table/i_idq/*}
+
 log -r /*
 run -a
